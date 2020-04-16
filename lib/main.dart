a9bb0e3010cb14da4b1b6f0d906bcc1ae7dba723
santosh <sklearn21@gmail.com>
2020-04-16 20:53:11 +0530

    Added encapsulation to the QuizBrain class.

--- lib/main.dart
+++ lib/main.dart
@@ -40,8 +40,6 @@ class _QuizPageState extends State<QuizPage> {
   Question q1 = Question(
       q: 'You can lead a cow down stairs but not up stairs.', a: false);*/
 
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -54,7 +52,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,8 +76,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.getCorrectAnswer();
                 if (correctAnswer == true) {
                   print('User got it right.');
                 } else {
@@ -87,7 +84,7 @@ class _QuizPageState extends State<QuizPage> {
                 }
                 setState(
                   () {
-                    questionNumber++;
+                    quizBrain.nextQuestion();
                   },
                 );
               },
@@ -107,8 +104,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.getCorrectAnswer();
+
                 if (correctAnswer == false) {
                   print('User got it right.');
                 } else {
@@ -116,7 +113,7 @@ class _QuizPageState extends State<QuizPage> {
                 }
                 setState(
                   () {
-                    questionNumber++;
+                    quizBrain.nextQuestion();
                   },
                 );
               },
